711569cc5a952e43220ae254fd28c048eb6a58b2
Eric A. Zarko <ezarko@roblox.com>
2020-12-14 22:04:33 -0800

    2020-12-15

---
+++ 2020/day15/part1.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+ my @numbers = (0,13,1,8,6,15);
+
+my %l = map {($numbers[$_] => $_)} 0..$#numbers;
+my $n = 0;
+my $c = @numbers;
+
+while ($c < 2019) {
+  my $o = exists($l{$n}) ? $c - $l{$n} : 0;
+  $l{$n} = $c++;
+  $n = $o;
+}
+
+print "$n\n";
